898f1a5ef0d1539f2e022eab466360198880aa10
LoftLoot <dean.aziz@gmail.com>
2025-12-06 13:07:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Hero from './components/Hero';
 import Footer from './components/Footer'; 
 import ProductCard from './components/ProductCard';
 import ProductDetail from './components/ProductDetail';
-import AboutSection from './components/About';
+import About from './components/About';
 
 // --- STYLES ---
 
@@ -483,7 +483,7 @@ const AppContent = () => {
                     </div>
                 </main>
             </>
-          ) : ( <AboutSection /> )}
+          ) : ( <About /> )}
         </>
       )}
       <Footer />
@@ -493,3 +493,4 @@ const AppContent = () => {
 
 const App = () => ( <ErrorBoundary><script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(ORGANIZATION_SCHEMA) }} /><AppContent /></ErrorBoundary> );
 export default App;
+
