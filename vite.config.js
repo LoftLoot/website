7454e587f3ff7dace35f5abd17118005a31354ae
LoftLoot <dean.aziz@gmail.com>
2025-12-12 16:38:30 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ isSsrBuild }) => ({
-  base: '/website/',
+  base: '/',
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom', 'react-router-dom'],
