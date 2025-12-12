76299b3348641ed7c0501f530da5d006a125c7c0
LoftLoot <dean.aziz@gmail.com>
2025-12-12 14:18:17 +0000

    Update prerender.jsx

--- prerender.jsx
+++ prerender.jsx
@@ -6,9 +6,16 @@ import React from 'react';
 export function prerender(url) {
   const basename = '/website/';
   
+  // FIX: Prepend the basename to the URL so the Router recognizes it.
+  // If url is "/", location becomes "/website/"
+  // If url is "/about", location becomes "/website/about"
+  const location = url.startsWith(basename) 
+    ? url 
+    : basename.replace(/\/$/, '') + url;
+  
   const html = renderToString(
     <React.StrictMode>
-      <StaticRouter basename={basename} location={url}>
+      <StaticRouter basename={basename} location={location}>
         <App />
       </StaticRouter>
     </React.StrictMode>
