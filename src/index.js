6468dc69ab1a9a00f86ab851ba16580202d598e7
LoftLoot <dean.aziz@gmail.com>
2025-12-12 12:43:19 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import { App } from './App'; // Named export
 
 const rootElement = document.getElementById('root');
+const baseUrl = import.meta.env.BASE_URL || '/website/';
+
+const ClientApp = () => (
+  <React.StrictMode>
+    <BrowserRouter basename={baseUrl}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
 if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, 
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  hydrateRoot(rootElement, <ClientApp />);
 } else {
   const root = createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  root.render(<ClientApp />);
 }
